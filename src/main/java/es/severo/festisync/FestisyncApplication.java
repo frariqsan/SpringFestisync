6e7ce0e127521311b7e3d00fcc9dd99583b0b3e6
frariqsan <frariqsan@gmail.com>
2025-12-26 14:28:20 +0100

    ADD: "First commit"

---
+++ src/main/java/es/severo/festisync/FestisyncApplication.java
@@ -0,0 +1,13 @@
+package es.severo.festisync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestisyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestisyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/severo/festisync/FestisyncApplicationTests.java
@@ -0,0 +1,13 @@
+package es.severo.festisync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestisyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
